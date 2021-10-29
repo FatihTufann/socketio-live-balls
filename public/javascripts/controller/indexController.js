94f40cbdbac5cc56cdf5237b8ca58aeaae2fef47
FatihTufann <20190311019@dogus.edu.tr>
2021-10-30 00:38:10 +0300

    chat connect user

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,11 +1,37 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    const connectionsOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 600
+
+    $scope.messages = [ ];
+
+    $scope.init = () => {
+      const username = prompt('please enter username');
+
+      if (username)
+          initSocket(username);
+      else
+          return false;
     };
-    indexFactory.connectSocket('http://localhost:3000', connectionsOptions).then((socket) => {
-        console.log('baglanti gerceklesti', socket);
-    }).catch((err) => {
-       console.log(err);
-    });
+
+    function initSocket(username){
+        const connectionsOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 600
+        };
+        indexFactory.connectSocket('http://localhost:3000', connectionsOptions)
+            .then((socket) => {
+                socket.emit('newUser', {username});
+
+                socket.on('newUser', (data) => {
+                    const messageData = {
+                        type: 0,
+                        username: data.username
+                    };
+
+                    $scope.messages.push(messageData);
+                    $scope.$apply();
+                });
+            }).catch((err) => {
+                console.log(err);
+            });
+    };
+
 }]);
