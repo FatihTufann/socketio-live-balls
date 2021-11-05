f9a16b143b1112dcd9a301797974591e01b293e9
FatihTufann <20190311019@dogus.edu.tr>
2021-11-05 17:31:42 +0300

    animation finished

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,6 +1,7 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
     $scope.messages = [ ];
+    $scope.players = { };
 
     $scope.init = () => {
       const username = prompt('please enter username');
@@ -20,15 +21,61 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
             .then((socket) => {
                 socket.emit('newUser', {username});
 
+                socket.on('initPlayers', (players) => {
+                    $scope.players = players;
+                    $scope.$apply();
+                });
+
                 socket.on('newUser', (data) => {
                     const messageData = {
-                        type: 0,
+                        type: {
+                            code: 0,
+                            message: 1
+                        },
                         username: data.username
                     };
 
                     $scope.messages.push(messageData);
+                    $scope.players[data.id] = data;
                     $scope.$apply();
                 });
+
+                socket.on('disUser', (user) => {
+                    const messageData = {
+                        type: {
+                            code: 0,
+                            message: 0
+                        },
+                        username: user.username
+                    };
+                   console.log(user);
+                   $scope.messages.push(messageData);
+                   delete $scope.players[user.id];
+                   $scope.$apply();
+                });
+
+                socket.on('animate', (data) => {
+                    $('#'+ data.socketId).animate({ 'left': data.x, 'top': data.y}, () => {
+                        animate = false;
+                    });
+                });
+
+                let animate = false;
+                $scope.onClickPlayer = ($event) => {
+                    if (!animate){
+                        let x = $event.offsetX;
+                        let y = $event.offsetY;
+                        socket.emit('animate',{ x, y});
+
+                        animate = true;
+                        $('#'+ socket.id).animate({ 'left': x, 'top': y}, () => {
+                            animate = false;
+                        });
+                    }
+
+
+                };
+
             }).catch((err) => {
                 console.log(err);
             });
--- src/socketApi.js
+++ src/socketApi.js
@@ -4,7 +4,10 @@ const io = socketio();
 const socketApi = { };
 socketApi.io = io;
 
-const users = [];
+const users = { };
+
+//helpers
+const randomColor = require('../helpers/randomColor');
 
 io.on('connection',(socket) => {
     console.log('a user connected');
@@ -15,13 +18,30 @@ io.on('connection',(socket) => {
            position: {
                x: 0,
                y: 0
-           }
+           },
+           color: randomColor()
        }
 
        const userData = Object.assign(data, defaultData);
-       users.push(userData);
+       users[socket.id] = userData;
+       console.log(users);
 
        socket.broadcast.emit('newUser', userData);
+       socket.emit('initPlayers', users);
+    });
+
+    socket.on('disconnect', () => {
+       socket.broadcast.emit('disUser', users[socket.id]);
+       delete users[socket.id];
+
+       console.log(users);
+    });
+
+    socket.on('animate', (data) => {
+       users[socket.id].position.x = data.x;
+       users[socket.id].position.y = data.y;
+
+       socket.broadcast.emit('animate', {socketId: socket.id, x: data.x, y: data.y});
     });
 });
 
